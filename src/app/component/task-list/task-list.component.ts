c97e3b482692291cdc9366edccdbe280b2ac7b18
fedepaz <fedepaz89@gmai.com>
2022-06-26 12:48:02 -0300

    continuando

--- src/app/component/task-list/task-list.component.ts
+++ src/app/component/task-list/task-list.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { TASKS } from 'src/app/mock-tasks';
 import { Task } from 'src/app/Task';
 import { faTimes } from '@fortawesome/free-solid-svg-icons';
+import { ServiceTaskService } from 'src/app/service-task.service';
 
 @Component({
   selector: 'app-task-list',
@@ -9,13 +9,19 @@ import { faTimes } from '@fortawesome/free-solid-svg-icons';
   styleUrls: ['./task-list.component.css']
 })
 export class TaskListComponent implements OnInit {
-  tasks:Task[]= TASKS;
+  tasks:Task[]= [];
   fatimes=faTimes;
   
 
-  constructor() { }
+  constructor(
+    private taskService:ServiceTaskService
+  ) { }
 
   ngOnInit(): void {
+    this.taskService.getTasks()
+    .subscribe(tasks=>{
+      this.tasks= tasks
+    })
   }
 
 }
